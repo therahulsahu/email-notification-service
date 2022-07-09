1d13c5cea23568286b2715fee95a7a7646f45e32
Rahul Sahu <therahulsahu7@gmail.com>
2022-07-09 20:20:21 +0530

    Added mail sending feature

---
+++ src/main/java/com/demo/emailnotification/EmailController.java
@@ -0,0 +1,17 @@
+package com.demo.emailnotification;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired private EmailService emailService;
+
+    @PostMapping("/sendMail")
+    public String sendMail(@RequestBody EmailDetails details) {
+        return emailService.sendSimpleMail(details);
+    }
+}
